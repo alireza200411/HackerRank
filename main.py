363468bc87c87c02847218c3c9125b2ef58b7c38
alireza200411 <alirezamoradi292048@gmail.com>
2025-01-02 17:57:33 +0330

    add 'write the function'

---
+++ main.py
@@ -0,0 +1,16 @@
+# author : alireza200411
+
+
+# 1. write the function
+def is_leap(year):
+    leap = False
+    if year % 4 == 0 and year % 100 != 0:
+        leap = True
+    elif year % 100 == 0:
+        if year % 400 == 0:
+            leap = True
+        else:
+            pass
+             
+    
+    return leap
