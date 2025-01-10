99abd08d9180ac9cf3de1e70a321f299ab033b0e
alireza200411 <alirezamoradi292048@gmail.com>
2025-01-10 20:01:16 +0330

    add Nested Lists

--- main.py
+++ main.py
@@ -14,3 +14,29 @@ def is_leap(year):
              
     
     return leap
+
+
+# 2. Nested Lists
+main_list = []
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    tuple_stu = (name, score)
+    list_stu = list(tuple_stu)
+    main_list.append(list_stu)
+    score_list = []
+for data in main_list:
+    score = data[1]
+    score_list.append(score)
+
+answer_list = []
+score_list = list(set(score_list))
+score_list.sort()
+answer = score_list[1]
+for data in main_list:
+    if data[1] == answer:
+        answer_list.append(data)
+answer_list.sort()
+
+for name in answer_list:
+    print(name[0])
